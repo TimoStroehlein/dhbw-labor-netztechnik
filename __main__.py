69ac25b076588a1c5a838ddafc46b895ee8a57db
timo <timostroehlein@gmail.com>
2020-03-20 14:11:43 +0100

    fixes and improvements

--- __main__.py
+++ __main__.py
@@ -6,6 +6,11 @@ from controller.Graph import Graph
 from model.Link import Link
 from model.Node import Node
 
+MAX_IDENT = 5
+MAX_ITEMS = 100000
+MAX_COST = 10000
+MAX_NODE_ID = 100000
+
 
 def read_file():
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'files/graph.st'), 'r') as file:
@@ -14,17 +19,37 @@ def read_file():
         counter = 0
         for line in file:
             counter += 1
+            # Check if the maximum number of items has been reached
+            if counter > MAX_ITEMS:
+                break
             # Check if line is node
-            result = re.search('^\\s*([a-zA-Z]+)\\s?=\\s?([0-9]*);.*', line)
+            result = re.search('^\\s*([a-zA-Z][a-zA-Z0-9]*)\\s?=\\s?([0-9]*);.*', line)
             if result:
+                # Check if name exceeds max length
+                if len(result.group(1)) > MAX_IDENT:
+                    continue
+                # Check if node id exceeds max number
+                if int(result.group(2)) > MAX_NODE_ID:
+                    continue
                 nodes.append(Node(int(result.group(2)), result.group(1)))
                 continue
             # Check if line is link
-            result = re.search('^\\s*([a-zA-Z]+)\\s?-\\s?([a-zA-Z])\\s?:\\s?([0-9]*);.*', line)
+            result = re.search('^\\s*([a-zA-Z][a-zA-Z0-9]*)\\s?-\\s?([a-zA-Z][a-zA-Z0-9]*)\\s?:\\s?([0-9]*);.*', line)
             if result:
+                # Check if name exceeds max length
+                if len(result.group(1)) > MAX_IDENT or len(result.group(2)) > MAX_IDENT:
+                    continue
+                # Check if link cost exceeds max cost
+                if int(result.group(3)) > MAX_COST:
+                    continue
                 links.append(Link(result.group(1), result.group(2), int(result.group(3))))
                 continue
+            # Check if line is comment
             result = re.search('//.*', line)
+            if result:
+                continue
+            # Check if line is empty
+            result = re.search('^\\s*$', line)
             if result:
                 continue
             print('Error in line (', counter, '): ', line)
@@ -33,8 +58,8 @@ def read_file():
 
 
 def print_graph(graph):
-    print('Node count: ', graph.nodes.__len__())
-    print('Link count: ', graph.links.__len__())
+    print('Node count:', graph.nodes.__len__())
+    print('Link count:', graph.links.__len__())
 
 
 def main():
@@ -43,16 +68,26 @@ def main():
     graph.find_links()
     # Test
     test = Test(graph)
-    if not test.check_node_ids_greater_zero():
+    result, msg = test.check_node_ids_greater_zero()
+    if not result:
+        print('Error on node with id', msg, ', ids cannot be 0 or less.')
         return
-    print('')
-    if not test.check_one_root_id_exists():
+    result, msg = test.check_one_root_id_exists()
+    if not result:
+        print('Root with id', msg, 'cannot exist multiple times.')
         return
-    if test.check_graph_connected() is not graph.nodes.__len__():
+    result, msg1, msg2 = test.check_node_connected_to_itself()
+    if not result:
+        print('Node with link', msg1, '-', msg2, 'cannot be connected to itself.')
+        return
+    result, msg1, msg2 = test.check_link_uniqueness()
+    if not result:
+        print('Link', msg1, '-', msg2, 'cannot exist more than once.')
+        return
+    if not test.check_graph_connected():
         print('Graph is not connected.')
         return
-    if not test.check_node_connected_to_itself():
-        return
+    print('All tests passed.')
     print_graph(graph)
     # Calculate
     graph.spann_tree(10)
--- controller/Test.py
+++ controller/Test.py
@@ -6,8 +6,8 @@ class Test:
     def check_node_ids_greater_zero(self):
         for node in self.graph.nodes:
             if node.node_id < 1:
-                return False
-        return True
+                return False, node.node_id
+        return True, 0
 
     def check_one_root_id_exists(self):
         node_ids = []
@@ -15,18 +15,26 @@ class Test:
             node_ids.append(node.node_id)
         node_ids.sort()
         if node_ids.__len__() > 1 and node_ids[0] is node_ids[1]:
-            return False
-        return True
+            return False, node_ids[0]
+        return True, 0
+
+    def check_node_connected_to_itself(self):
+        for link in self.graph.links:
+            if link.node_id_1 is link.node_id_2:
+                return False, link.node_id_1, link.node_id_2
+        return True, '', ''
 
     def check_link_uniqueness(self):
         for i1 in range(len(self.graph.links)):
             for i2 in range(len(self.graph.links)):
+                if i1 is i2:
+                    continue
                 l1 = self.graph.links[i1]
                 l2 = self.graph.links[i2]
                 if (l1.node_id_1 is l2.node_id_1 and l1.node_id_2 is l2.node_id_2) or \
-                        (l1.node_id_1 is l2.node_id_2 and l2.node_id_2 is l1.node_id_1):
-                    return False
-        return True
+                        (l1.node_id_1 is l2.node_id_2 and l1.node_id_2 is l2.node_id_1):
+                    return False, l1.node_id_1, l1.node_id_2
+        return True, '', ''
 
     def check_graph_connected(self, node=None):
         if node is None:
@@ -45,16 +53,4 @@ class Test:
                 for node2 in self.graph.nodes:
                     if node2.name is link.node_id_1:
                         self.check_graph_connected(node2)
-        return self.visited_node_ids.__len__()
-
-    def find_visited_node(self, node_id):
-        for visited_node_id in self.visited_node_ids:
-            if visited_node_id is node_id:
-                return True
-        return False
-
-    def check_node_connected_to_itself(self):
-        for link in self.graph.links:
-            if link.node_id_1 is link.node_id_2:
-                return False
-        return True
+        return len(self.graph.nodes) is len(self.visited_node_ids)
